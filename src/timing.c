d83a6c955df7d4c4cc2aa18dedc0f9b3d70dbf4b
Mike Gran <spk121@yahoo.com>
2018-01-11 04:51:01 -0800

    assume ftime will not be used
    * config.h.in (HAVE_FTIME): removed
    * configure.ac (AC_CHECK_FUNC): remove ftime check
    * src/timing.c (timing): use gettimeofday

--- src/timing.c
+++ src/timing.c
@@ -31,33 +31,8 @@
 
 
 /* Prefer gettimeofday to ftime to times.  */
-#  undef HAVE_FTIME
 #  undef HAVE_TIMES
 
-#ifdef HAVE_TIMES
-#    include <sys/times.h>
-#  ifdef _SC_CLK_TCK
-#    define HAVE_SC_CLK_TCK 1
-#  else
-#    define HAVE_SC_CLK_TCK 0
-#  endif
-/* TIMES_TICK may have been set in policy.h, or we may be able to get
-   it using sysconf.  If neither is the case, try to find a useful
-   definition from the system header files.  */
-#  if !defined(TIMES_TICK) && (!defined(HAVE_SYSCONF) || !defined(HAVE_SC_CLK_TCK))
-#    ifdef CLK_TCK
-#      define TIMES_TICK CLK_TCK
-#    else /* ! defined (CLK_TCK) */
-#      ifdef HZ
-#        define TIMES_TICK HZ
-#      endif /* defined (HZ) */
-#    endif /* ! defined (CLK_TCK) */
-#else
-#  endif /* TIMES_TICK == 0 && (! HAVE_SYSCONF || ! HAVE_SC_CLK_TCK) */
-#  ifndef TIMES_TICK
-#    define TIMES_TICK 0
-#  endif
-#endif /* HAVE_TIMES */
 
 #ifdef HAVE_GETTIMEOFDAY
 /* collides with Solaris 2.5 prototype? */
@@ -69,8 +44,6 @@ timing (int reset, time_t *nowp)
 {
   static double elaptime, starttime, stoptime;
   double yet;
-#define NEED_TIME
-#ifdef HAVE_GETTIMEOFDAY
   struct timeval tv;
   struct timezone tz;
 
@@ -81,67 +54,7 @@ timing (int reset, time_t *nowp)
 #endif
   gettimeofday (&tv, &tz);
   yet=tv.tv_sec + tv.tv_usec/1000000.0;
-#undef NEED_TIME
-#endif
-#ifdef HAVE_FTIME
-	static int fbad=0;
 
-	if (! fbad)
-	{
-		struct timeb stime;
-		static struct timeb slast;
-
-		(void) ftime (&stime);
-
-		/* On some systems, such as SCO 3.2.2, ftime can go backwards in
-		   time.  If we detect this, we switch to using time.  */
-		if (slast.time != 0
-			&& (stime.time < slast.time
-			|| (stime.time == slast.time && stime.millitm < slast.millitm)))
-			fbad = 1;
-		else
-		{
-			yet = stime.millitm / 1000.0  + stime.time;
-			slast = stime;
-		}
-	}
-	if (fbad)
-		yet=(double) time(NULL);
-#undef NEED_TIME
-#endif
-
-#ifdef HAVE_TIMES
-  struct tms s;
-  long i;
-  static int itick;
-
-  if (itick == 0)
-    {
-#if TIMES_TICK == 0
-#if HAVE_SYSCONF && HAVE_SC_CLK_TCK
-      itick = (int) sysconf (_SC_CLK_TCK);
-#else /* ! HAVE_SYSCONF || ! HAVE_SC_CLK_TCK */
-      const char *z;
-
-      z = getenv ("HZ");
-      if (z != NULL)
-        itick = (int) strtol (z, (char **) NULL, 10);
-
-      /* If we really couldn't get anything, just use 60.  */
-      if (itick == 0)
-        itick = 60;
-#endif /* ! HAVE_SYSCONF || ! HAVE_SC_CLK_TCK */
-#else /* TIMES_TICK != 0 */
-      itick = TIMES_TICK;
-#endif /* TIMES_TICK == 0 */
-    }
-  yet = ((double) times (&s)) / itick;
-#undef NEED_TIME
-#endif
-
-#ifdef NEED_TIME
-	yet=(double) time(NULL);
-#endif
   if (nowp)
     *nowp=(time_t) yet;
   if (reset) {
