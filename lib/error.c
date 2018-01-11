a6d212d56bb2d888fc967a047bf0330f64f4eb00
Mike Gran <spk121@yahoo.com>
2018-01-11 11:58:15 -0800

    Assume vprintf exists in stdio.h
    * configure.ac (AC_CHECK_FUNCS): remove vprintf test
    * lib/error.c: modified

--- lib/error.c
+++ lib/error.c
@@ -23,25 +23,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #include <stdio.h>
 
-#if HAVE_VPRINTF || HAVE_DOPRNT || _LIBC
-# if __STDC__
-#  include <stdarg.h>
-#  define VA_START(args, lastarg) va_start(args, lastarg)
-# else
-#  include <varargs.h>
-#  define VA_START(args, lastarg) va_start(args)
-# endif
-#else
-# define va_alist a1, a2, a3, a4, a5, a6, a7, a8
-# define va_dcl char *a1, *a2, *a3, *a4, *a5, *a6, *a7, *a8;
-#endif
+#include <stdarg.h>
+#define VA_START(args, lastarg) va_start(args, lastarg)
 
-#if STDC_HEADERS || _LIBC
-# include <stdlib.h>
-# include <string.h>
-#else
-void exit ();
-#endif
+#include <stdlib.h>
+#include <string.h>
 
 #ifndef _
 # define _(String) String
@@ -59,37 +45,8 @@ void (*error_print_progname) (
 /* This variable is incremented each time `error' is called.  */
 unsigned int error_message_count;
 
-#ifdef _LIBC
-/* In the GNU C library, there is a predefined variable for this.  */
-
-# define program_name program_invocation_name
-# include <errno.h>
-
-#else
-
-/* The calling program should define program_name and set it to the
-   name of the executing program.  */
-extern char *program_name;
-
-# if HAVE_STRERROR
-#  ifndef strerror		/* On some systems, strerror is a macro */
-char *strerror ();
-#  endif
-# else
-static char *
-private_strerror (errnum)
-     int errnum;
-{
-  extern char *sys_errlist[];
-  extern int sys_nerr;
-
-  if (errnum > 0 && errnum <= sys_nerr)
-    return sys_errlist[errnum];
-  return _("Unknown system error");
-}
-#  define strerror private_strerror
-# endif	/* HAVE_STRERROR */
-#endif	/* _LIBC */
+#define program_name program_invocation_name
+#include <errno.h>
 
 /* Print the program name and error message MESSAGE, which is a printf-style
    format string with optional args.
@@ -98,19 +55,9 @@ private_strerror (errnum)
 /* VARARGS */
 
 void
-#if defined(VA_START) && __STDC__
 error (int status, int errnum, const char *message, ...)
-#else
-error (status, errnum, message, va_alist)
-     int status;
-     int errnum;
-     char *message;
-     va_dcl
-#endif
 {
-#ifdef VA_START
   va_list args;
-#endif
 
   if (error_print_progname)
     (*error_print_progname) ();
@@ -120,17 +67,9 @@ error (status, errnum, message, va_alist)
       fprintf (stderr, "%s: ", program_name);
     }
 
-#ifdef VA_START
   VA_START (args, message);
-# if HAVE_VPRINTF || _LIBC
   vfprintf (stderr, message, args);
-# else
-  _doprnt (message, args, stderr);
-# endif
   va_end (args);
-#else
-  fprintf (stderr, message, a1, a2, a3, a4, a5, a6, a7, a8);
-#endif
 
   ++error_message_count;
   if (errnum)
@@ -146,22 +85,10 @@ error (status, errnum, message, va_alist)
 int error_one_per_line;
 
 void
-#if defined(VA_START) && __STDC__
 error_at_line (int status, int errnum, const char *file_name,
 	       unsigned int line_number, const char *message, ...)
-#else
-error_at_line (status, errnum, file_name, line_number, message, va_alist)
-     int status;
-     int errnum;
-     const char *file_name;
-     unsigned int line_number;
-     char *message;
-     va_dcl
-#endif
 {
-#ifdef VA_START
   va_list args;
-#endif
 
   if (error_one_per_line)
     {
@@ -188,17 +115,9 @@ error_at_line (status, errnum, file_name, line_number, message, va_alist)
   if (file_name != NULL)
     fprintf (stderr, "%s:%d: ", file_name, line_number);
 
-#ifdef VA_START
   VA_START (args, message);
-# if HAVE_VPRINTF || _LIBC
   vfprintf (stderr, message, args);
-# else
-  _doprnt (message, args, stderr);
-# endif
   va_end (args);
-#else
-  fprintf (stderr, message, a1, a2, a3, a4, a5, a6, a7, a8);
-#endif
 
   ++error_message_count;
   if (errnum)
